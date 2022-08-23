1b284be2d94674eb5800460d8349a7625e0ea1af
NadiiaZ <zashikhina93@gmail.com>
2022-08-23 19:05:16 +0700

    done

---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat{
+    private String message;
+    public Cat() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
